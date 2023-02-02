46b3126d6e2e000fdcadcf7842edf461c1710447
janmajaya <janmajaya2000@gmail.com>
2023-02-02 16:54:50 +0530

    First commit

---
+++ Website landing page_0/script.js
@@ -0,0 +1,25 @@
+
+gsap.to(".imagecontainer",{
+    width:"100%",
+    ease:Expo.easeInOut,
+    stagger:2
+    // repeat:-1
+})
+
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    stagger:2,
+    top:0
+    // repeat:-1
+
+})
+
+gsap.to(".text h1",{
+    delay:2,
+    ease:Expo.easeInOut,
+    stagger:2,
+    top:"-100%"
+    // repeat:-1
+   
+
+})
